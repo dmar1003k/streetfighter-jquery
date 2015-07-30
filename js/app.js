e2df4593982996133c03f568c35ee1e7e5b1d307
dmar1003k <ddc0714@gmail.com>
2015-07-29 19:46:06 -0700

    Ryu Throwing Position

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// play hadouken sound
+		// show hadouken and animate it to the right of the screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+	.mouseup(function() {
+		// ryu goes back to his readu position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});

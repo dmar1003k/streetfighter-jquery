b51aaf8e7f0904f30d26467bbced06dc1af63730
dmar1003k <dd524c@gmail.com>
2015-08-04 22:49:23 -0700

    keydown, keyup

--- js/app.js
+++ js/app.js
@@ -22,24 +22,27 @@ $(document).ready(function() {
 		);
 	})
 	.mouseup(function() {
-		// ryu goes back to his readu position
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
-	.keydown(function(e) {
-		if (e.keyCode == 88) {
-			$('.ryu-ready').hide();
-			$('.ryu-cool').show();
-		};			
-	})
-	.keyup(function(e) {
-		if (e.keyCode == 88) {
-			$('.ryu-cool').hide();
-			$('.ryu-ready').show();
-		};		 					
 	});
 
+	$(document)
+	.keydown(function(e) {
+		if(e.keyCode == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(e) {
+		if(e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').show();
+		}
+	});
 });
+
+
+
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;

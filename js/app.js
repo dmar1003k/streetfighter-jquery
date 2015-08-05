39f400ae818647d0d7c9f9449f0a6fb20e98dc54
dmar1003k <dd524c@gmail.com>
2015-08-04 21:58:29 -0700

    e.key

--- js/app.js
+++ js/app.js
@@ -3,20 +3,47 @@ $(document).ready(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	.mouseleave(function(){
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
-		// show hadouken and animate it to the right of the screen
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function() {
 		// ryu goes back to his readu position
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+	})
+	.keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		};			
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').show();
+		};		 					
 	});
+
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
